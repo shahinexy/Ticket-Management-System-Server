c2e9fedc1e8478e2fe6cf9447d20ce0cf49fb882
shahinexy <shahinexy@gmail.com>
2024-11-28 22:26:49 +0600

    .env and .gitignore file created

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,9 +9,9 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_PASS)
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.76h69in.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.76h69in.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
