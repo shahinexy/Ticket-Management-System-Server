37a3b68197b699116babbdac752956a30a18e95c
shahinexy <shahinexy@gmail.com>
2024-12-02 11:50:07 +0600

    get data route created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,7 +9,6 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_PASS)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.76h69in.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -39,6 +38,19 @@ async function run() {
       }
       const result = await userCollection.insertOne(newUser);
       res.send(result)
+      console.log('user post success');
+    })
+
+    app.get('/allUser', async (req, res) =>{
+      const result = await userCollection.find().toArray();
+      return res.send(result)
+    })
+
+    app.get('/allUser/:id', async (req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.findOne(query);
+      return res.send(result)
     })
 
 
